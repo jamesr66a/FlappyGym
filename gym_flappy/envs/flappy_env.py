3805fd4b0fdf344860518734c7a95dda0663a8ea
James Reed <6jamesr6@gmail.com>
2016-11-07 00:21:15 -0500

    attribs

--- gym_flappy/envs/flappy_env.py
+++ gym_flappy/envs/flappy_env.py
@@ -4,9 +4,12 @@ from gym.utils import seeding
 
 class FlappyEnv(gym.Env):
   metadata = {'render.modes': ['human']}
+  (w, h) = (640, 480)
+
 
   def __init__(self):
-    print 'init'
+    self.action_space = spaces.Tuple((spaces.discrete(2)))
+    self.observation_space = spaces.Box(low=-1, high=1, shape=(w,h))
 
   def _step(self, action):
     print 'step'
